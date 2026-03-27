315869757d328467b75acb017a5166c75610bf97
Sanchita Das <Sanchita.Das@cognizant.com>
2026-03-28 00:58:32 +0530

    fixes in course

--- course_service/src/main/java/com/ctx/course_service/dto/CourseResponseDTO.java
+++ course_service/src/main/java/com/ctx/course_service/dto/CourseResponseDTO.java
@@ -2,6 +2,7 @@ package com.ctx.course_service.dto;
 
 import jakarta.validation.constraints.NotBlank;
 
+import java.util.List;
 import java.util.UUID;
 
 public record CourseResponseDTO (
@@ -11,6 +12,7 @@ public record CourseResponseDTO (
     @NotBlank String description,
     @NotBlank  String courseCode,
     @NotBlank   Double duration,
-    @NotBlank UUID teacherId
+    @NotBlank UUID teacherId,
+    List<ModuleResponseDTO> moduleResponseDTO
 ){
 }
