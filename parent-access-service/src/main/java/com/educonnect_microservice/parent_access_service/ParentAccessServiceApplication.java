c399a16c5f6b54764438da9d350e6ded1fc1b17c
A B Ramya <2479308@cognizant.com>
2026-03-27 15:30:20 +0530

    added parent-access-service

---
+++ parent-access-service/src/main/java/com/educonnect_microservice/parent_access_service/ParentAccessServiceApplication.java
@@ -0,0 +1,13 @@
+package com.educonnect_microservice.parent_access_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParentAccessServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParentAccessServiceApplication.class, args);
+	}
+
+}
