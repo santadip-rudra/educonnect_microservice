71de4513fa6521773700ac7935d19e6f48cba802
A B Ramya <2479308@cognizant.com>
2026-04-20 16:37:23 +0530

    parent

---
+++ student-registry-service/src/main/java/com/ctx/student_registry_service/config/JacksonConfig.java
@@ -0,0 +1,17 @@
+package com.ctx.student_registry_service.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+    @Autowired
+    public void configureObjectMapper(ObjectMapper objectMapper) {
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    }
+}
