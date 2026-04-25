8950a39b5ece67ab282f2e7ba3a65dea6a5f1f87
Sanchita Das <Sanchita.Das@cognizant.com>
2026-04-26 00:07:31 +0530

    course search

--- course_service/src/main/java/com/ctx/course_service/service/implementation/CourseServiceImpl.java
+++ course_service/src/main/java/com/ctx/course_service/service/implementation/CourseServiceImpl.java
@@ -199,4 +199,12 @@ public class CourseServiceImpl implements CourseService {
     public List<CourseCompletionStatsDTO> getCourseCompletionStats(){
         return entityManagerRepo.getCourseCompletionStats();
     }
+
+    @Override
+    public List<CourseResponseDTO> searchCourse(String keyword) {
+         return  courseRepo.findByTitleContainingIgnoreCase(keyword)
+                 .stream()
+                 .map(course -> courseMapper.toResponseDTO(course))
+                 .toList();
+    }
 }
